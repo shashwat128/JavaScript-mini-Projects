4b303689d42eb875a8305c5663df92000101334d
Shashwat Singh <shashwat2048@gmail.com>
2023-06-21 22:02:12 +0530

    7- flip a coin: script.js

--- AsmrProg/7-Flip a Coin Game/script.js
+++ AsmrProg/7-Flip a Coin Game/script.js
@@ -6,15 +6,15 @@ let resetBtn = document.querySelector("#reset-button");
 
 flipBtn.addEventListener("click", () => {
   let i = Math.floor(Math.random() * 2);
-  coin.computedStyleMap.animation = "none";
+  coin.style.animation = "none";
   if (i) {
     setTimeout(function () {
-      coin.computedStyleMap.animation = "spin-heads 3s forwards";
+      coin.style.animation = "spin-heads 3s forwards";
     }, 100);
     heads++;
   } else {
     setTimeout(function () {
-      coin.computedStyleMap.animation = "spin-tails 3s forwards";
+      coin.style.animation = "spin-tails 3s forwards";
     }, 100);
     tails++;
   }
@@ -23,19 +23,19 @@ flipBtn.addEventListener("click", () => {
 });
 
 function updateStats() {
-  document.querySelector("heads-count").textContent = `Heads: ${heads}`;
-  document.querySelector("tails-count").textContent = `Tails: ${tails}`;
+  document.querySelector("#heads-count").textContent = `Heads: ${heads}`;
+  document.querySelector("#tails-count").textContent = `Tails: ${tails}`;
 }
 
-const disabledButton = function () {
+function disableButton() {
   flipBtn.disabled = true;
   setTimeout(function () {
     flipBtn.disabled = false;
   }, 3000);
-};
+}
 
 resetBtn.addEventListener("click", () => {
-  coin.computedStyleMap.animation = "none";
+  coin.style.animation = "none";
   heads = 0;
   tails = 0;
   updateStats();
