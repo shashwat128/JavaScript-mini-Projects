dae5d200152b07ba2018d6fcb8e655edf4082d92
Shashwat Singh <shashwat2048@gmail.com>
2023-08-02 13:30:50 +0530

    game start, score logic

--- Games/6. Dinosaur Run (Chrome)/script.js
+++ Games/6. Dinosaur Run (Chrome)/script.js
@@ -2,15 +2,21 @@ import { setupGround, updateGround } from "./ground.js";
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
+const SPEED_INCREMENT = 0.000004;
 
 const worldElem = document.querySelector("[data-world]");
+const scoreElem = document.querySelector("[data-score]");
+const startScreenElem = document.querySelector("[data-start-screen");
 
 setPixelToWorldScale();
 window.addEventListener("resize", setPixelToWorldScale);
+document.addEventListener("keydown", handleStart, { once: true });
 
 setupGround();
 
 let lastTime;
+let speedScale;
+let score;
 function update(time) {
   if (lastTime == null) {
     lastTime = time;
@@ -19,12 +25,31 @@ function update(time) {
   }
   const delta = time - lastTime;
 
-  updateGround(delta, 1);
+  updateGround(delta, speedScale);
+  updateSpeedScale(delta);
+  updateScore(delta);
 
   lastTime = time;
   window.requestAnimationFrame(update);
 }
-window.requestAnimationFrame(update);
+
+function updateSpeedScale(delta) {
+  speedScale += delta * SPEED_INCREMENT;
+}
+
+function updateScore(delta) {
+  score += delta * 0.01;
+  scoreElem.textContent = Math.floor(score);
+}
+
+function handleStart() {
+  lastTime = null;
+  speedScale = 1;
+  score = 0;
+  setupGround();
+  startScreenElem.classList.add("hide");
+  window.requestAnimationFrame(update);
+}
 
 function setPixelToWorldScale() {
   let worldToPixelScale;
