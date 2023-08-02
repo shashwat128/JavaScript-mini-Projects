4c765b426c742fa661750e5895f8aadf51caa78a
Shashwat Singh <shashwat2048@gmail.com>
2023-08-02 12:31:00 +0530

    looping ground movement

--- Games/6. Dinosaur Run (Chrome)/script.js
+++ Games/6. Dinosaur Run (Chrome)/script.js
@@ -1,3 +1,5 @@
+import { setupGround, updateGround } from "./ground.js";
+
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
 
@@ -5,6 +7,8 @@ const worldElem = document.querySelector("[data-world]");
 
 setPixelToWorldScale();
 window.addEventListener("resize", setPixelToWorldScale);
+
+setupGround();
 
 let lastTime;
 function update(time) {
@@ -14,7 +18,8 @@ function update(time) {
     return;
   }
   const delta = time - lastTime;
-  console.log(delta);
+
+  updateGround(delta, 1);
 
   lastTime = time;
   window.requestAnimationFrame(update);
